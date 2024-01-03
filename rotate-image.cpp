838c0b9317cc231c36f676ed628ed232afffdbd1
Naman Agrawal <122742832+GitnamanHub@users.noreply.github.com>
2024-01-03 10:01:37 +0530

    Add files via upload

---
+++ rotate-image.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        for(int i=0;i<n/2;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                swap(matrix[j][i],matrix[j][n-i-1]);
+            }
+        }
+    }
+
+int main()
+{
+    vector<vector<int>> matrix={{1,2,3},
+                                {4,5,6},
+                                {7,8,9}};
+    rotate(matrix);
+    int n=matrix.size();
+    for(int i=0;i<n;i++)
+    {
+        for(int x:matrix[i])
+        {
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
